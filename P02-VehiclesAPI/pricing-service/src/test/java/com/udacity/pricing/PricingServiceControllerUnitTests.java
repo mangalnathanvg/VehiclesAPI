d5587bb22cfba9b701e78050e789b38348c8a90d
Mangalnathan Vijayagopal <mangalnathanvg@gmail.com>
2022-03-18 13:30:26 -0400

    Convert the pricing service to a microservice and add additional unit test

---
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceControllerUnitTests.java
@@ -0,0 +1,37 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingServiceControllerUnitTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    PricingService pricingService;
+
+    @Test
+    public void getPriceofVehicle() throws Exception{
+        mockMvc.perform(get("/services/price?vehicleId=2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        Mockito.verify(pricingService, times(1)).getPrice(2L);
+    }
+}
